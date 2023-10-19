05aaa106c5f3560ee299b122128f41d68a7c2672
pocachip <parkseou@gmail.com>
2023-10-19 18:05:04 +0900

    good

--- frontend/ngx-admin/src/app/pages/rightwatch/contents-list/contents-list.component.ts
+++ frontend/ngx-admin/src/app/pages/rightwatch/contents-list/contents-list.component.ts
@@ -11,8 +11,7 @@ import { SmartTableData } from '../../../@core/data/smart-table';
   styleUrls: ['./contents-list.component.scss']
 })
 export class ContentsListComponent { 
-
-    settings = {
+  settings = {
     add: {
       addButtonContent: '<i class="nb-plus"></i>',
       createButtonContent: '<i class="nb-checkmark"></i>',
@@ -32,74 +31,19 @@ export class ContentsListComponent {
         title: 'ID',
         type: 'number',
       },
-      website: {
-        title: 'website',
-        type: 'number',
-      },
-/*      cat1_code: {
-        title: 'cat1_code',
-        type: 'string',
-      },
-      cat2_code: {
-        title: 'cat2_code',
-        type: 'string',
-      },
-      */
-      cat1_title: {
-        title: 'cat1_title',
-        type: 'string',
-      },
-      cat2_title: {
-        title: 'cat2_title',
-        type: 'string',
-      },
-      idx: {
-        title: 'idx',
-        type: 'string',
-      },
-      txt: {
-        title: 'txt',
-        type: 'string',
-      },
-      lvl19: {
-        title: 'lvl19',
-        type: 'string',
-      },
-      payment: {
-        title: 'payment',
-        type: 'string',
-      },
-      seller: {
-        title: 'seller',
-        type: 'string',
-      },
-      partnership: {
-        title: 'partnership',
-        type: 'string',
-      },
-      attach_file_size: {
-        title: 'attach_file_size',
-        type: 'string',
-      },
-      item_url: {
-        title: 'item_url',
-        type: 'string',
-      },
-      last_update: {
-        title: 'last_update',
+      title: {
+        title: 'contents_name',
         type: 'string',
       },
     },
+    pager: {
+      display : true,
+      perPage: 20,
+    },
   };
 
-  source1: LocalDataSource = new LocalDataSource();
   source: ServerDataSource;
 
-/*  constructor(private service: SmartTableData) {
-    const data = this.service.getData();
-    this.source.load(data);
-  }
-  */
  /*
  conf: ServerSourceConf;
     protected static readonly SORT_FIELD_KEY = "_sort";
@@ -111,19 +55,38 @@ export class ContentsListComponent {
     protected static readonly DATA_KEY = "";
 */
  conf ={ 
-    endPoint: "http://127.0.0.1:5555/api/v1/post?limit=",
-    sortFieldKey: "cat1_code",
-    sortDirKey: "order",
-    pagerPageKey: "offset",
-    pagerLimitKey: "limit",
+    endPoint: "http://127.0.0.1:5555/api/v1/contents-list",
     totalKey: "total",
     dataKey: "data",
+    pagerPageKey: "offset",
+    pagerLimitKey: "limit",
+    /*
+    sortFieldKey: "id",
+    sortDirKey: "order",
+    */
   }
 
   constructor(http: HttpClient) {
-//    this.source = new ServerDataSource(http, { endPoint: 'https://jsonplaceholder.typicode.com/photos' });
+   //  this.source = new ServerDataSource(http, { endPoint: 'http://127.0.0.1:5555/api/v1/contents-list' });
     this.source = new ServerDataSource(http, this.conf)
   }
+  onSearch(query: string = '') {
+  this.source.setFilter([
+    // fields we want to include in the search
+    {
+      field: 'id',
+      search: query
+    },
+    {
+      field: 'title',
+      search: query
+    },
+  ], false); 
+  // second parameter specifying whether to perform 'AND' or 'OR' search 
+  // (meaning all columns should contain search query or at least one)
+  // 'AND' by default, so changing to 'OR' by setting false here
+  }
+
 
   onDeleteConfirm(event): void {
     if (window.confirm('Are you sure you want to delete?')) {
