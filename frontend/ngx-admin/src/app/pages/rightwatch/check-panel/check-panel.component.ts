ec75551c92cbb2e7a0c6e179e0ea60e5c5012a05
pocachip <parkseou@gmail.com>
2023-10-23 09:02:44 +0900

    dash

--- frontend/ngx-admin/src/app/pages/rightwatch/check-panel/check-panel.component.ts
+++ frontend/ngx-admin/src/app/pages/rightwatch/check-panel/check-panel.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnDestroy, ViewChild} from '@angular/core';
+import { Component, OnDestroy, Directive, Input, ViewChild } from '@angular/core';
 import { takeWhile } from 'rxjs/operators';
+
+@Directive({selector: 'pane'})
+export class Pane {
+  @Input() id!: string;
+}
 
 @Component({
   selector: 'ngx-check-panel',
   templateUrl: './check-panel.component.html',
   styleUrls: ['./check-panel.component.scss']
 })
+
 export class CheckPanelComponent implements OnDestroy {
+  @ViewChild(Pane)
+    set pane(v: Pane) {
+    setTimeout(() => {
+      this.selectedPane = v.id;
+    }, 0);
+  }
+  selectedPane: string = '';
+  shouldShow = true;
+  toggle() {
+    this.shouldShow = !this.shouldShow;
+  }
 
   private alive = true;
 
